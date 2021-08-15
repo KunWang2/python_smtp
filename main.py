60fe4dbae2d1518c41d623fb1c171e721cd5881b
unknown <459354755@qq.com>
2021-08-15 19:25:22 +0800

    Mod: 提交conf配置信息

--- main.py
+++ main.py
@@ -3,32 +3,42 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 import json
+import sys
 
 
 class SMTPConfig():
     def __init__(self, path, conf_type=""):
-        f = open(path, 'r')
+        f = open(path, 'r', encoding='utf-8')
         smtp_str = f.read()
         self.select = conf_type
         self.info = json.loads(smtp_str)
+        self.dict_info = self.info.get(conf_type)
+        if self.dict_info is None:
+            print("Please Check the Conf Type ")
         print("Step 2: Init The Configure Info")
 
     def GetMailHostServer(self):
+        return self.dict_info["hostserver"]
         print()
 
     def GetMailTitle(self):
+        return self.dict_info["title"]
         print()
 
     def GetMailReceiver(self):
+        return self.dict_info["receiver"]
         print()
 
     def GetMailSender(self):
+        return self.dict_info["sender"]
         print()
 
     def GetMailSenderPwd(self):
+        return self.dict_info["password"]
         print()
 
     def GetMailContent(self):
+        return self.dict_info["content"]
         print()
 
 
@@ -38,12 +48,12 @@ class SMTPManger():
         print("Init The Confure Info")
         self.__config = SMTPConfig(conf_path, conf_type)
 
-    def CreateSMTPInfo(self, type=""):
+    def CreateSMTPInfo(self):
         msg = MIMEMultipart()
         # 1. 建立邮箱的标题
         msg["Subject"] = Header(self.__config.GetMailTitle(), 'utf-8')
         # 2. 建立邮箱的发件人
-        msg["From"] = self.__config.GetMailReceiver()
+        msg["From"] = self.__config.GetMailSender()
         # 3. 建立邮箱的收件人
         msg['To'] = self.__config.GetMailReceiver()
         # 4. 建立邮箱的正文
@@ -79,7 +89,13 @@ if __name__ == '__main__':
 
     print("Step 1: Init The SMTP Manager")
 
-    manager = SMTPManger()
+    path = "./conf/mail.json"
+    # 修改成入参指定
+    select_type = "marsol"
+    if len(sys.argv) > 1 :
+        select_type = sys.argv[1]
+
+    manager = SMTPManger(path, select_type)
     manager.SendSMTP()
 
     print("Finish")
